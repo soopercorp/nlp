3f9c41b948b9c6af7df6bd3eb6dad6c3c500b28f
Hardik Ruparel <hardik988@gmail.com>
2012-01-21 02:49:22 -0800

    added sample output and more rules in part1.py

--- 2012/nlp-hw1/code/part1.py
+++ 2012/nlp-hw1/code/part1.py
@@ -3,29 +3,53 @@
 import random
 
 def main():
-	num_grammar_rules = 10
+	
+	# GRAMMAR
 	grammar = []
 	grammar.append("S")					# 0
 	grammar.append(["NP","VP"]) 		# 
 	grammar.append("S")					# 2
-	grammar.append(["NP"]) 				# 
-	grammar.append("NP")				# 4
-	grammar.append(["N","PP"]) 			# 
-	grammar.append("NP")				# 6	
-	grammar.append(["V","PP"]) 			# 
-	grammar.append("VP")				# 8	
-	grammar.append(["V","PP"])			# 
-	grammar.append("N")					# 10
-	grammar.append(["boy","girl"])		# 
-	grammar.append("V")					# 12
-	grammar.append(["screw","do"])		# 
-	grammar.append("PP")				# 14
-	grammar.append(["in","on"])	
+	grammar.append(["AUX","NP","VP"]) 	#
+	grammar.append("S")					# 4
+	grammar.append(["NP","AUX","VP"]) 	#
+	grammar.append("NP")				# 6
+	grammar.append(["DET","ADJ","N","PP"])
+	grammar.append("NP")				# 8
+	grammar.append(["N","PP"]) 			
+	grammar.append("NP")				# 10
+	grammar.append(["PRO"]) 			#
+	grammar.append("VP")				# 12
+	grammar.append(["V","NP"])			#
+	grammar.append("VP")				# 14
+	grammar.append(["V","NP","PP"])		
+	grammar.append("VP")				# 16
+	grammar.append(["V","ADV"])			#
 	
+	# LEXICON
+	grammar.append("AUX")				
+	grammar.append(["does","can","may","will"])		
+	grammar.append("N") 
+	grammar.append(["boy","girl","house","car"]) 
+	grammar.append("V")
+	grammar.append(["fly","kick","kiss"]) 
+	grammar.append("PP")
+	grammar.append(["in","on","between"])	
+	grammar.append("PRO")
+	grammar.append(["I","you","he","it"])	
+	grammar.append("DET") 
+	grammar.append(["a","an","the"])
+	grammar.append("ADJ") 
+	grammar.append(["awesome","good","bad"])
+	grammar.append("ADV") 
+	grammar.append(["slowly","fast","incredibly"])
+
 
 	stack = ["S"]
 	sentence = []
 
+	"""
+	Selects a random rule from one of the matching rules. 
+	"""
 	def selectRandomRule(rule):
 		i = -1
 		selected = []
@@ -35,15 +59,23 @@ def main():
 				selected.append(i)
 		except ValueError:
 			pass
-		selectedRule = random.sample(selected,1)[0]
+		selectedRule = random.sample(selected,1)[0] # Selects a random element from list
 		return selectedRule
 	
+	
+	"""
+	Selects a random word from one of the lexicon definitions.
+	"""
 	def selectRandomWord(ruleNumber):
-		return random.sample(grammar[ruleNumber+1],1)[0]
+		return random.sample(grammar[ruleNumber+1],1)[0] # returns the string form of selected random word
 
+	
+	"""
+	Expand the stack.
+	"""
 	def expand(stack):
 		selectedRule = selectRandomRule(stack[len(stack)-1])
-		if(selectedRule <= 8):
+		if(selectedRule <= 16):
 			print "Expanding rule number: {}".format(selectedRule)
 			print "Executing rule: "+ grammar[selectedRule]+"-> "+str(grammar[selectedRule+1])
 			stack.pop() # Pop the rule just executed
@@ -55,20 +87,13 @@ def main():
 			sentence.append(word)
 			print "Appended word : "+word+" to sentence"
 		
-	i = 0
 	while stack:
 		expand(stack)
 		print "Stack is now: "
 		print stack
-		i = i + 1
 	
-	# Now stack is expanded for first time
-
 	print "Sentence is : "
 	print sentence
-
-	
-	
 		
 if __name__ == '__main__':
 	main()
