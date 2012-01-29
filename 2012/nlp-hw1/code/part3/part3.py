11fd14bfe84557f5f353cee04c87581f6a264d6e
Hardik Ruparel <hardik988@gmail.com>
2012-01-29 13:25:11 -0800

    added lex

--- 2012/nlp-hw1/code/part3/part3.py
+++ 2012/nlp-hw1/code/part3/part3.py
@@ -3,6 +3,7 @@
 # @copyright All Rights Reserved
 
 from collections import OrderedDict
+from itertools import chain
 from sys import stdout
 import pprint
 
@@ -28,8 +29,22 @@ def main():
 					stdout.write(k)
 					print " {0} ".format(v),
 				print "\n"
-		
-		
+	
+	"""Determines if current rule is in lexicon or not
+	Eg : isInLexicon(N[])
+	"""
+	def isInLexicon(rule):
+		for w in [item for sublist in lexicon["N"] for item in sublist]:
+		if(w == word):
+			return 1
+
+
+	"""
+	Parses the grammar passed with the frame included
+	Eg: Parse("NP[head:agent]",frame)
+	"""
+	def Parse(grammar,frame):
+		pass
 	# initialize list of grammar rules
 	g = []
 	
@@ -38,7 +53,7 @@ def main():
 				OrderedDict([("AUX",["head","agent"]),("NP",["head","agent"]),("VP",["head"])]),\
 				OrderedDict([("DET",["head","determiner"]),("ADJ*",["head"]),("N",["head","type"]),("PP*",["head"])])]
 	
-	# add S
+	# add rules for S
 	g.append(Grammar("S",rhsRule)) 
 
 	# loop over grammar and print rules
@@ -55,7 +70,26 @@ def main():
 
 	pp.pprint(frame)
 
-	# lexicon
-		
+	# lexicon format
+	# N : [[singular,plural]]
+	# V : [[present, past]]
+	# AUX : [[present, present-neg, past, past-neg]]
+	# ADJ : [list of adj]
+	# ADV : [list of adv]
+	lexicon = {}
+
+	lexicon = { 
+	  "N":[['potato',"potatoes"],["In-N-Out","In-N-Out"],["french-fry","french-fries"]],\
+	  "V": [["cook","cooked"],["prepare","prepared"],["use","used"],["deliver","delivered"],["take","took"]],\
+	  "AUX": [["can","cannot","could","could not"],["should","shouldn't","should have","shouldn't have"],\
+	   		 ["does","doesn't","did","didn't"],["will","won't","would","wouldn't"]],\
+	  "ADJ": ["old","new","tasty","fresh","good","whole","trans-fat-free","vegetable","hot"],\
+	  "ADV": ["individually"],
+	  "PREP": ["at","that"]
+	}
+
+	#pp.pprint(lexicon)
+	
+	
 if __name__ == '__main__':
 	main()
