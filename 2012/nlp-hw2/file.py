3a03f38a00b24033b01fb3d7401b3ffb4dd83124
Hardik Ruparel <hardik988@gmail.com>
2012-02-25 16:54:14 -0800

    generating arff files now

--- 2012/nlp-hw2/file.py
+++ 2012/nlp-hw2/file.py
@@ -1,31 +1,91 @@
 #!/usr/bin/env python
 
 import re
+import pprint
+import string
 
-file = "/home/hr/Desktop/kozareva-assignment-2/eng.train.bio"
+pp = pprint.PrettyPrinter(indent=4)
 
-fd = open(file)
+testFile = "/home/hr/study/nlp/2012/nlp-hw2/eng.train.bio"
+funcFile = "/home/hr/study/nlp/2012/nlp-hw2/prep.txt"
+arffFile = "/home/hr/study/nlp/2012/nlp-hw2/features.arff"
+
+puncs = ['.',',','!','(',')','-',':',';','~','--','"','?','$',"''"]
+
+#open testfile
+fd = open(testFile)
 contents = fd.readlines()
 fd.close()
 
-def org(entity):
-	if(entity[2]!= "O"):
-		if(entity[2].split("-")[1] == "ORG"):
-			return 1
-		else:
-			return 0
+#open functional word file
+funcFd = open(funcFile)
+funcs = funcFd.readlines()
+funcFd.close()
+
+#strip \n
+contents = map(string.strip,contents)
+funcs = map(string.strip,funcs)
+
+# features
+# [posTag,isAllCaps,firstLetterCaps,wordLength,isFuncWord,isPunctuation
+# contatinsDots, ]
+
+features = []
 
 def extractFeature(entity):
-	if(org(entity)):
-		pass
-		print entity[0] + " is " + entity[2]
+	feature = []
+
+	feature.append(entity[0])
+	#posTag
+	if entity[1] not in puncs:
+		feature.append(entity[1])
+	else:
+		feature.append('?')
+
+	#allCaps
+	if entity[0].isupper():
+		feature.append('1')
+	else:
+		feature.append('0')	
+
+	#firstCaps
+	if(entity[0][0].isupper()):
+		feature.append('1')
+	else:
+		feature.append('0')
+
+	#word length
+	feature.append(str(len(entity[0])))
+
+	#functional word = determiner,conjunction,preposition
+	feature.append(str(int(entity[0] in funcs)))
+
+	#punctuation mark
+	feature.append(str(int(entity[0] in puncs)))
+
+	#contains dots
+	if(entity[0] != '.'):
+		feature.append(str(int('.' in entity[0])))
+	else:
+		feature.append('0')
+
+	#class
+	feature.append(entity[2])
+
+	features.append(feature)
 
 for line in contents:
 	if not line.strip():
 		continue
 	else:
 		entity = line.rstrip().split()
-		if entity[0] == "." or entity[0] == ",":
-			continue
-		else:
-			extractFeature(entity)
+		extractFeature(entity)
+
+arff = open(arffFile,"a")
+
+# remove entity word from list of features and join rest by ,
+for line in features:
+	line.pop(0)
+	arff.write((','.join(line))+'\n')
+
+arff.close()
